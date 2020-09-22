d52b3f0138ca03c952dde60e733be9d97d182785
Fahim Muntasir <55046623+fahim25@users.noreply.github.com>
2020-09-22 23:55:54 +0600

    Added jQ for mobbile Nav

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/*
+==================================================
+|	|	Navigation
+==================================================
+*/
+$(function () {
+	$('#feature').waypoint(function(direction){
+		if(direction == "down"){
+			$('nav').addClass('sticky');
+		}
+		else{
+			$('nav').removeClass('sticky');
+		}
+	},{
+		offset: '100px'	
+	});
+});
+/*
+==================================================
+|	|	Mobile Navigation
+==================================================
+*/
+$(function () {
+	$('.js--nav').click(function(){
+		 var nav = $('.js--main--nav');
+		 var icon = $('.js--nav ion-icon');
+		
+		nav.slideToggle(200);
+		
+		//if(icon.hasClass('menu-outline')){
+		//	icon.addClass('close-outline');
+		//	icon.removeClass('menu-outline');
+		//}
+		//else
+		//{
+		//	icon.addClass('menu-outline');
+		//	icon.removeClass('close-outline');
+		//}
+	});
+});
+	
+	
